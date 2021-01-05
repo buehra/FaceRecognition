86ec8fa90ca812d939f7004dfd3d65e6f0e99cdc
Raphael Bühlmann <raphael.buehlmann@hotmail.com>
2021-01-05 18:21:56 +0100

    Added eye detection

--- face-recognition.py
+++ face-recognition.py
@@ -3,10 +3,12 @@ import sys
 
 # Get user supplied values
 imagePath = "images/test.jpg"
-cascPath = "data/haarcascade_frontalface_default.xml"
+faceCascPath = "data/haarcascade_frontalface_default.xml"
+eyeCascPath = "data/haarcascade_eye.xml"
 
 # Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPath)
+faceCascade = cv2.CascadeClassifier(faceCascPath)
+eyeCascade = cv2.CascadeClassifier(eyeCascPath)
 
 # Read the image
 image = cv2.imread(imagePath)
@@ -15,7 +17,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Detect faces in the image
 faces = faceCascade.detectMultiScale(
     gray,
-    scaleFactor=1.1,
+    scaleFactor=1.3,
     minNeighbors=5,
     minSize=(30, 30)
     #flags = cv2.CV_HAAR_SCALE_IMAGE
@@ -26,6 +28,12 @@ print("Found {0} faces!".format(len(faces)))
 # Draw a rectangle around the faces
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    eye_gray = gray[y:y+h, x:x+w]
+    eye_color = image[y:y+h, x:x+w]
+        
+    eyes = eyeCascade.detectMultiScale(eye_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(eye_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
